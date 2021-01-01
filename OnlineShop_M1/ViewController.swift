4817ad431ad80c89ccb83a4cdba17d0d418abf55
edgarkosyan <e.kosyan0073@gmail.com>
2021-01-01 22:32:37 +0400

    Initial Commit

---
+++ OnlineShop_M1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineShop_M1
+//
+//  Created by edgar kosyan on 01.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
